8a6a7a7a1aa3c50fa82b8fcd9795ab332fd852cc
Mostak Ahmed <mosak.ahm263@gmail.com>
2024-01-22 21:56:00 +0600

    setup the server with proper error handling

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sentinel Trust Bank Server
+
+- Follow  the example.env file to integrate confidential data.
+- Use .env file for succesfull execution of the server.
+- Run the command before executing the code
+    ```
+        npm install
+    ```
---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const globalErrorHandler = require('./utils/globalErrorHandler');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+// default route
+app.get('/', async (req, res) => {
+    try {
+        res.status(200).send('SENTINEL TRUST BANK SERVER IS ONLINE......')
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
+})
+
+
+// handle all unhandled routes
+app.all('*', async (req, res, next) => {
+    const error = new Error(`Can't find ${req.originalUrl} on the server`)
+    error.status = 404;
+    next(error);
+})
+
+
+// error handling middleware
+app.use(globalErrorHandler)
+
+
+
+const main = async => {
+    app.listen(port, () => {
+        console.log(`server is listening to ${port}`)
+    })
+}
+
+main();
