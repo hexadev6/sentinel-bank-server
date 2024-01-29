070eee30ba09bb8eb64f62318e5c1edb8ec8ecc0
Mostak Ahmed <mosak.ahm263@gmail.com>
2024-01-30 03:10:21 +0600

    added a two routes on user accounts

--- src/routes/account/index.js
+++ src/routes/account/index.js
@@ -1,8 +1,12 @@
 const createAccount = require('../../api/account/controllers/createAccount');
+const getUserAccounts = require('../../api/account/controllers/getUserAccounts');
 
 const router = require('express').Router();
 
 
-router.post('/createAccount', createAccount);
+router.post('/createBankAccount', createAccount);
+router.get('/userAccounts', getUserAccounts)
+// router.get
+// router.patch
 
 module.exports = router
