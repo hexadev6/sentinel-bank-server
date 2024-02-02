cfaa9e4c2e6add3153b6454aa96f3d1e87421209
Zannatul Ferdousi Hema <shanzidadisha1234.com>
2024-02-02 16:46:13 +0600

    total amount api

--- src/routes/account/index.js
+++ src/routes/account/index.js
@@ -6,9 +6,11 @@ const {
   getUserAccounts,
 } = require("../../api/account/controllers");
 const getAllAccounts = require("../../api/account/controllers/getAllAccounts");
+const findByAccNum = require("../../api/account/controllers/findByAccount");
 const router = require("express").Router();
 
 router.get("/findBankAccountOne/:id", findBankAccountOne);
+router.get("/findByAccNum/:accNum", findByAccNum);
 router.get("/allAccountUser", allAccountUser);
 router.patch("/updateAccount/:id", updateAccount);
 router.post("/createBankAccount", createAccount);
