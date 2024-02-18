8ef975ede4d37a650faec1c02ec4c3bf6b9b6ed6
Zannatul Ferdousi Hema <shanzidadisha1234.com>
2024-02-18 21:23:24 +0600

    chatapp push error

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors= require("cors")
-const http = require("http");
-const socketIo = require("socket.io");
 const globalErrorHandler = require("./utils/globalErrorHandler");
 // start chat
 const chatServer = require('./api/chatapp/liveChat/chatServer')
@@ -13,7 +11,6 @@ const http = require('http');
 const server = http.createServer(app);
 // end chat
 const port = process.env.PORT || 5000;
-const authRoutes = require("./routes/authentication");
 const connectDB = require("./db/connectDB");
 const authRoutes = require("./routes/authentication");
 const userRoutes = require("./routes/userInfo/userInfo");
@@ -73,65 +70,3 @@ const main = async () => {
 main();
 
 
-
-
-
-
-
-
-// const express = require("express");
-// const globalErrorHandler = require("./utils/globalErrorHandler");
-// const applyMiddleware = require("./middlewares");
-// const app = express();
-// const port = process.env.PORT || 5000;
-// const authRoutes = require("./routes/authentication");
-// const connectDB = require("./db/connectDB");
-// const userRoutes = require("./routes/userInfo/userInfo");
-// const accountRoutes = require("./routes/account");
-// const paymentRoutes = require("./routes/payment");
-// const transactionsRoutes = require("./routes/transactions");
-// const applicationCardRoutes = require("./routes/applicationCard");
-// const allDeposits = require("./routes/transactions");
-// const reviewRoutes = require("./routes/review");
-// const noticeRoutes = require("./routes/notice");
-
-
-// // applying all middlewares like cors, express json, cookie parser
-// applyMiddleware(app);
-
-// app.use(authRoutes);
-// app.use(userRoutes);
-// app.use(accountRoutes);
-// app.use(paymentRoutes);
-// app.use(transactionsRoutes);
-// app.use(applicationCardRoutes);
-// app.use(reviewRoutes);
-// app.use(noticeRoutes);
-
-// // default route
-// app.get("/", async (req, res) => {
-//   try {
-//     res.status(200).send("SENTINEL TRUST BANK SERVER IS ONLINE......");
-//   } catch (error) {
-//     res.status(500).send({ message: error.message });
-//   }
-// });
-
-// // handle all unhandled routes
-// app.all("*", async (req, res, next) => {
-//   const error = new Error(`Can't find ${req.originalUrl} on the server`);
-//   error.status = 404;
-//   next(error);
-// });
-
-// // error handling middleware
-// app.use(globalErrorHandler);
-
-// const main = async () => {
-//   await connectDB();
-//   app.listen(port, () => {
-//     console.log(`server is listening to ${port}`);
-//   });
-// };
-
-// main();
