3d4278796d7652b97a359f8874e3559b84580605
Israt85 <isratanjuman3@gmail.com>
2024-02-23 00:16:04 +0600

    added trasaction summary

--- src/api/account/controllers/getTrasactionSummary.js
+++ src/api/account/controllers/getTrasactionSummary.js
@@ -4,14 +4,13 @@ const Withdraw = require('../../../models/WithdrawHistoryModal');
 const transactionSummary = async (req, res) => {
     const accNum = parseInt(req.params?.accNum);
     console.log(accNum);
-    
 
     try {
-        const depositSummary = await Deposit.aggregate([
+        const summary = await Deposit.aggregate([
             {
                 $match: {
-                    acc_Num: accNum, 
-                    transactionType: 'deposit'
+                    acc_Num: accNum,
+                    transactionType: { $in: ['deposit', 'withdraw'] }
                 }
             },
             {
@@ -21,31 +20,54 @@ const transactionSummary = async (req, res) => {
                         transactionDate: '$transactionDate',
                         amount: '$amount',
                         accNum: '$acc_Num'
-                        
                     },
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    depositSummary: {
+                        $push: {
+                            $cond: {
+                                if: { $eq: ['$_id.transactionType', 'deposit'] },
+                                then: '$_id',
+                                else: null
+                            }
+                        }
+                    },
+                    withdrawSummary: {
+                        $push: {
+                            $cond: {
+                                if: { $eq: ['$_id.transactionType', 'withdraw'] },
+                                then: '$_id',
+                                else: null
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    depositSummary: {
+                        $filter: {
+                            input: '$depositSummary',
+                            as: 'deposit',
+                            cond: { $ne: ['$$deposit', null] }
+                        }
+                    },
+                    withdrawSummary: {
+                        $filter: {
+                            input: '$withdrawSummary',
+                            as: 'withdraw',
+                            cond: { $ne: ['$$withdraw', null] }
+                        }
+                    }
                 }
             }
         ]);
 
-        // const withdrawSummary = await Withdraw.aggregate([
-        //     {
-        //         $group: {
-        //             _id: {
-        //                 transactionType: '$transactionType',
-        //                 transactionDate: '$transactionDate'
-        //             },
-        //             totalAmount: { $sum: '$amount' },
-        //             count: { $sum: 1 }
-        //         }
-        //     }
-        // ]);
-
-        const summary = {
-            depositSummary,
-            // withdrawSummary
-        };
-
-        res.json(summary);
+        res.json(summary[0]);
 
     } catch (error) {
         console.error(error);
