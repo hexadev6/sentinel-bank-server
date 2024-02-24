4ae74c21d2aa2d08f327e15ebcb4521d4ee15087
Zannatul Ferdousi Hema <shanzidadisha1234@gmail.com>
2024-02-24 22:01:24 +0600

    merge after conflict fix

--- src/models/chatModel.js
+++ src/models/chatModel.js
@@ -13,9 +13,4 @@ const chatSchema = new Schema({
 
 const ChatModel = model('Chat', chatSchema);
 
-<<<<<<< HEAD
 module.exports = ChatModel;
-=======
-
-module.exports = ChatModel;
->>>>>>> 6338b7182082ea4709662104afa082a9a9c3b492
